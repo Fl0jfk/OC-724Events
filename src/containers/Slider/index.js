56340fc9df939b1d712ce4d667898a6bf8cb40ee
Flojfk <florian.hacqueville@hotmail.fr>
2023-07-21 16:22:22 +0200

    debug radioIndex slider

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { useData } from "../../contexts/DataContext";
 import { getMonth } from "../../helpers/Date";
-
 import "./style.scss";
 
 const Slider = () => {
@@ -23,30 +22,20 @@ const Slider = () => {
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
         <>
-          <div
-            key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
+          <div key={event.title} className={`SlideCard SlideCard--${index === idx ? "display" : "hide"}`}>
             <img src={event.cover} alt="forum" />
             <div className="SlideCard__descriptionContainer">
               <div className="SlideCard__description">
                 <h3>{event.title}</h3>
                 <p>{event.description}</p>
                 <div>{getMonth(new Date(event.date))}</div>
-              </div>
+              </div> 
             </div>
           </div>
           <div className="SlideCard__paginationContainer">
             <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
-                <input
-                  key={`${event.id}`}
-                  type="radio"
-                  name="radio-button"
-                  checked={idx === radioIdx}
-                />
+              {byDateDesc.map((radioEvent, radioIdx) => (
+                <input key={`${radioEvent}`} type="radio" name="radio-button" checked={index === radioIdx}/>
               ))}
             </div>
           </div>
