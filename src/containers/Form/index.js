03e53f640ec4e3a41597ad3541cee613eb8af575
Flojfk <florian.hacqueville@hotmail.fr>
2023-08-02 16:44:19 +0200

    debug form ok

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -16,6 +16,7 @@ const Form = ({ onSuccess, onError }) => {
       try {
         await mockContactApi();
         setSending(false);
+        onSuccess(); // Pour corriger il suffisait d'appeler la fonction onSucess();
       } catch (err) {
         setSending(false);
         onError(err);
